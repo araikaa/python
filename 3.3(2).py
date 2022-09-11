886380c96bb06c9dabd1ee6e07256edb483e8ec2
araikaa <97833932+araikaa@users.noreply.github.com>
2022-09-11 13:04:50 +0300

    Add files via upload

---
+++ 3.3(2).py
@@ -0,0 +1,10 @@
+import math
+x=3
+y=0.2
+print('x=',x)
+print('y=',y)
+z=math.pow(x,2)
+a=math.exp(z)
+b=math.sqrt((math.pow(math.cos(y),2))-math.pow(y,2))
+c=(5*x*y)/(math.pow(x,3)-4)
+print(c+a+b)            
