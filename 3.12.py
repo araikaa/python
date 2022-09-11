0aa51946e615db403bd149736827fa9a283d36bc
araikaa <97833932+araikaa@users.noreply.github.com>
2022-09-11 14:56:06 +0300

    Add files via upload

---
+++ 3.12.py
@@ -0,0 +1,8 @@
+import math
+x=2
+y=1
+z=3
+print('x=',x)
+print('y=',y)
+print('z=',z)
+print((math.fabs(math.log(math.pow(x,3)))+math.exp(2*x))/(x+3.4)-(math.pow(math.cos(3/x*y*z)/math.sin(3/x*y*z),3)))
